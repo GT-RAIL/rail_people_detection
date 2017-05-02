92ba3d58fa97d4eecb9a65db6d5ee213eaef40ce
Siddhartha Banerjee <banerjs.sid@gmail.com>
2017-05-02 09:15:41 -0400

    Beginning the run through all the README files

---
+++ src/rail_person_detector/person_detector.py
@@ -0,0 +1,134 @@
+#!/usr/bin/env python
+# Module that receives people bounding boxes from the object detector and
+# publishes the positions of people in the image
+
+import rospy
+import numpy as np
+
+from math import isnan
+from struct import unpack_from
+from threading import Lock
+
+from rail_object_detector.msg import Detections as ObjectDetections
+from rail_person_detector.msg import People, Person, Centroid
+
+from rail_person_detector.position_mapper import CentroidPositionMapper
+
+class PersonDetector(object):
+    """
+    Takes the output from the object detector, and splits the output stream into
+    separate topics - one for the objects and another for the people. The people
+    have corresponding X,Y,Z information as well in the form of
+    geometry_msgs/PointStamped
+    """
+
+    def __init__(self):
+        """
+        Initialize the parameters and the internal class to call the conversion
+        functions
+        """
+
+        # Parameters
+        self.object_detector_topic = rospy.get_param(
+            "~object_detector_topic",
+            "/object_detector/detections"
+        )
+        self.desired_position_frame = rospy.get_param(
+            "~desired_position_frame",
+            "base_link"
+        )
+        self.position_match_threshold = rospy.get_param(
+            "~position_match_threshold",
+            None
+        )
+
+        # Initialize the converter
+        self.mapper = CentroidPositionMapper(False)
+
+        # Output topic
+        self.objects_topic = rospy.Publisher('~objects', ObjectDetections,
+                                             queue_size=10)
+        self.people_topic = rospy.Publisher('~people', People, queue_size=10)
+
+        # Input topic
+        self.detections_listener = rospy.Subscriber(
+            self.object_detector_topic,
+            ObjectDetections,
+            self._handle_detections
+        )
+
+        # Helper methods and variables
+        self.distance_func = lambda A,B : np.sqrt((A.x-B.x)**2 + (A.y-B.y)**2)
+        self.area_func = lambda A: (
+            (A.right_top_x-A.left_bot_x) * (A.left_bot_y-A.right_top_y)
+        )
+
+    def _get_valid_people(self, positions, candidates):
+        """
+        :param positions: list of positions of people
+        :param candidates: list of bounding boxes of people
+        :return: list of positions and bounding boxes that are valid
+        """
+        valid_people = []
+        for idx,pos in enumerate(positions):
+            if (np.isnan(pos.point.x)
+                or np.isnan(pos.point.y)
+                or np.isnan(pos.point.z)
+            ):
+                continue
+
+            matched = False
+            if self.desired_position_frame == 'base_link' and pos.point.z > 1.524:
+                # Silva hack: Centroid is above 5 ft. Buzz is messing with us
+                continue
+            if self.position_match_threshold is not None:
+                for i,valid in enumerate(valid_people):
+                    if (self.distance_func(valid.position.point, pos.point) <= self.position_match_threshold):
+                        if (self.area_func(valid.bounding_box) < self.area_func(candidates[idx].bounding_box)):
+                            valid_people[i] = candidates[idx]
+                            valid_people[i].position = pos
+                        # Assume only one match possible
+                        matched = True
+                        break
+
+            # True when no match AND when no threshold is present
+            if not matched:
+                candidates[idx].position = pos
+                valid_people.append(candidates[idx])
+
+        return valid_people
+
+    def _handle_detections(self, detections):
+        """
+        :param detections:
+        :return: nothing
+        """
+        objects_msg = ObjectDetections(header=detections.header)
+        people_msg = People()
+        candidate_people = []
+
+        # Loop through objects and add to the people array or the objects array
+        for obj in detections.objects:
+            if obj.label == 'person':
+                candidate_people.append(
+                    Person(bounding_box=obj, bb_header=detections.header)
+                )
+            else:
+                objects_msg.objects.append(obj)
+
+        # Transform the people
+        positions = self.mapper.convert_centroids_to_point([
+            Centroid(p.bounding_box.centroid_x, p.bounding_box.centroid_y)
+            for p in candidate_people
+        ], self.desired_position_frame)
+
+        # If there was some TF error, ABORT
+        if positions is None:
+            return
+
+        # Validate the people in the message
+        people_msg.people = self._get_valid_people(positions, candidate_people)
+
+        # Publish the resulting topics
+        self.people_topic.publish(people_msg)
+        self.objects_topic.publish(objects_msg)
---
+++ src/rail_person_detector/position_mapper.py
@@ -0,0 +1,184 @@
+#!/usr/bin/env python
+# Module that takes a maps a point in an image to the desired reference frame
+
+import rospy
+import tf
+import numpy as np
+
+from math import isnan
+from struct import unpack_from
+from threading import Lock
+
+from geometry_msgs.msg import PointStamped, Point
+from sensor_msgs.msg import PointCloud2
+from rail_person_detector.srv import CentroidPositionQuery, \
+    CentroidPositionQueryResponse
+
+class CentroidPositionMapper(object):
+    """
+    This class takes the centroid of an object as X,Y pixel values and
+    returns the position of that object in the desired reference frame.
+    Requires that the point cloud be present
+    """
+
+    def __init__(self, use_service=True):
+        """
+        Initialize parameters and listeners
+        :param use_service: Boolean for whether a separate service should be
+            initialized to communicate with this node
+        """
+        self.use_service = use_service
+
+        # Parameters
+        self.pcl_topic = rospy.get_param("~kinect_pcl_topic",
+                                         "/kinect/qhd/points")
+        self.pcl_frame = rospy.get_param("~kinect_pcl_frame",
+                                         "kinect_rgb_optical_frame")
+        self.transform_wait_timeout = rospy.get_param(
+            "~transform_wait_timeout", 2.0)
+
+        # Storage
+        self.point_cloud = None # Stored point cloud
+        self.pc_point_size = None
+        self.pc_row_size = None
+        self.pc_width = None
+        self.pc_height = None
+        self.point_cloud_lock = Lock()
+
+        # Listeners
+        self.tf_listener = tf.TransformListener()
+        self.pcl_listener = rospy.Subscriber(self.pcl_topic, PointCloud2,
+                                             self._save_point_cloud)
+        if self.use_service:
+            self.query_server = rospy.Service("~query", CentroidPositionQuery,
+                                              self._handle_centroid_query)
+
+    def _save_point_cloud(self, point_cloud):
+        """
+        Stores the point cloud for future processing as needed
+        :param point_cloud:
+        :return: nothing
+        """
+        if self.point_cloud_lock.acquire(False):
+            self.point_cloud = point_cloud
+            self.pc_point_size = point_cloud.point_step
+            self.pc_row_size = point_cloud.row_step
+            self.pc_width = point_cloud.width
+            self.pc_height = point_cloud.height
+            self.point_cloud_lock.release()
+
+    def convert_centroids_to_point(self, centroids, desired_frame):
+        """
+        Handles requests for centroid locations
+        :param centroids: List of centroids that we need a position for
+        :param desired_frame: Desired TF frame for the output positions
+        :return: List of positions of the corresponding centroids. None if fail
+        """
+
+        # Calculate the position of the corresponding point in the point cloud
+        pcl_points = []
+        self.point_cloud_lock.acquire()
+        if self.point_cloud is None:
+            self.point_cloud_lock.release()
+            return []
+        header = self.point_cloud.header # Use this header later
+        for centroid in centroids:
+
+            # Check that the point is not at the boundaries
+            if (centroid.x >= self.pc_width-1
+                or centroid.x < 0
+                or centroid.y >= self.pc_height-1
+                or centroid.y < 0
+            ):
+                pcl_points.append(None)
+                continue
+
+            # The position of the point is actually the mean of nearby points
+            neighbours = [(centroid.x, centroid.y),
+                          (centroid.x, centroid.y+1),
+                          (centroid.x+1, centroid.y),
+                          (centroid.x+1, centroid.y+1)]
+
+            # Only use those points for which we do have positions
+            num_valid = 0
+            neighbour_pos = np.empty((len(neighbours), 3))
+            for neighbour in neighbours:
+                x_idx = (self.pc_row_size * neighbour[1]
+                         + neighbour[0] * self.pc_point_size)
+                y_idx = (self.pc_row_size * neighbour[1]
+                         + neighbour[0] * self.pc_point_size + 4)
+                z_idx = (self.pc_row_size * neighbour[1]
+                         + neighbour[0] * self.pc_point_size + 8)
+                float_x = unpack_from('f', self.point_cloud.data, x_idx)
+                float_y = unpack_from('f', self.point_cloud.data, y_idx)
+                float_z = unpack_from('f', self.point_cloud.data, z_idx)
+
+                if isnan(float_x[0]) or isnan(float_y[0]) or isnan(float_z[0]):
+                    continue
+
+                neighbour_pos[num_valid, :] =\
+                    (float_x[0], float_y[0], float_z[0])
+                num_valid += 1
+
+            # Don't consider this point if none of the nearby points have a
+            # position in the point cloud
+            if num_valid == 0:
+                pcl_points.append(None)
+            else:
+                pcl_points.append(
+                    np.mean(neighbour_pos[0:num_valid,:], axis=0)
+                )
+
+        self.point_cloud_lock.release()
+
+        # Calculate the transformed position for all the valid points. For
+        # the rest, return a None object that has a timestamp of 0 and values
+        # of NAN
+        response = []
+        for pcl_point in pcl_points:
+
+            # Handle the case when the point is invalid
+            if pcl_point is None:
+                zp = PointStamped()
+                zp.point = Point(*([float('nan')] * 3))
+                response.append(zp)
+            else: # Transform the point to the desired reference frame
+                try:
+                    self.tf_listener.waitForTransform(
+                        self.pcl_frame,
+                        desired_frame,
+                        rospy.Time(0),
+                        rospy.Duration(self.transform_wait_timeout)
+                    )
+                    (trans, rot) = self.tf_listener.lookupTransform(
+                        self.pcl_frame,
+                        desired_frame,
+                        rospy.Time(0)
+                    )
+                except tf.Exception as e:
+                    rospy.logerr("Error transforming %s to %s: %s"
+                                 % (self.pcl_frame, desired_frame, str(e)))
+                    return None
+
+                pcl_R_desired =\
+                    tf.transformations.quaternion_matrix(rot)[0:3,0:3]
+                pcl_p_desired = (np.dot(pcl_point, pcl_R_desired)
+                                 - np.dot(trans, pcl_R_desired))
+
+                tp = PointStamped(header=header, point=Point(*pcl_p_desired))
+                tp.header.frame_id = desired_frame
+                response.append(tp)
+
+        # Return the response
+        return response
+
+    def _handle_centroid_query(self, req):
+        """
+        Handles requests for centroid locations
+        :param req: List of centroids that we need a position for
+        :return: A CentroidPositionQueryResponse object or None if failed
+        """
+        response = self.convert_centroids_to_point(req.centroids,
+                                                   req.desired_frame)
+        # Return the response object
+        return CentroidPositionQueryResponse(response)
